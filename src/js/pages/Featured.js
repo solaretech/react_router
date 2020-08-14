93cdcf8e65a5f5efb9101a86bda8ed0e0ced8f5e
solaretech <solaretech.kk@gmail.com>
2020-08-14 15:17:23 +0900

    complete migration from HTML to JavaScript

--- src/js/pages/Featured.js
+++ src/js/pages/Featured.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import Article from "../components/Article";
 
 export default class Featured extends React.Component {
   render() {
+    const Articles = [
+      "Some Article",
+      "Some Other Article",
+      "Yet Another Article",
+      "Still More",
+      "Some Article",
+      "Some Other Article",
+      "Yet Another Article",
+      "Still More",
+      "Some Article",
+      "Some Other Article",
+      "Yet Another Article",
+      "Still More"
+    ].map((title, i) => <Article key={i} title={title} />);
+
+    const AdText = [
+      "Ad spot #1",
+      "Ad spot #2",
+      "Ad spot #3",
+      "Ad spot #4",
+      "Ad spot #5"
+    ];
+
+    const randomAd = AdText[Math.round(Math.random() * (AdText.length - 1))];
+
     return (
-      <h1>Featured</h1>
+      <div>
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="well text-center">
+              {randomAd}
+            </div>
+          </div>
+        </div>
+        <div class="row">{Articles}</div>
+      </div>
     );
   }
 }
--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -1,21 +1,26 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
+import Nav from "../components/layout/Nav";
+import Footer from "../components/layout/Footer";
 
 class Layout extends React.Component {
-  navigate() {
-    console.log(this.props.history);
-    this.props.history.push("/");
-  }
   render() {
+    const { location } = this.props;
+    const containerStyle = {
+      marginTop: "60px"
+    };
     return (
       <div>
-        <h1>KillerNews.net</h1>
-        {this.props.children}
-        <Link to="/archives/some-other-articles" class="btn btn-warning">archives (some other articles)</Link>
-        <Link to="/archives" class="btn btn-danger">archives</Link>
-        <Link to="/settings/main" class="btn btn-success">settings</Link>
-        <Link to="/settings/extra" class="btn btn-success">settings (extra)</Link>
-        <button class="btn btn-info" onClick={this.navigate.bind(this)}>featured</button>
+        <Nav location={location} />
+        <div class="container" style={containerStyle}>
+          <div class="row">
+            <div class="col-lg-12">
+              <h1>KillerNews.net</h1>
+              {this.props.children}
+            </div>
+          </div>
+          <Footer/>
+        </div>
       </div>
     );
   }
